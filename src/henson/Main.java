14f85f72cec528019d9cfc64badf39b18a7a87b1
dragonx2907 <all21005@byui.edu>
2021-05-15 19:00:13 -0600

    Prove 04 Assignment

--- src/henson/Main.java
+++ src/henson/Main.java
@@ -3,5 +3,6 @@ package henson;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jonathan");
+        System.out.println("Hello Jonathan, my name is Bryce");
     }
 }
